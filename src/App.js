249f3b0b4fc4afa2fc0f495b5813bc2fb8a99fb2
chloe zheng <chloezxy@hotmail.com>
2019-11-09 15:56:58 +0800

    working on homepage

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import Homepage from './Homepage'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
 
-      </header>
-    </div>
+    //   </header>
+    // </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/" component={Homepage} exact/>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
